9e81dd1d7b6111a3fb282bfbb62ff7b95801f20a
lokenathghatakcs25-beep <lokenath.ghatak_cs25@gla.ac.in>
2026-03-18 14:01:44 +0530

    [lokenath] added Packages

---
+++ college/student/Students.java
@@ -0,0 +1,7 @@
+package PackageAssignment.college.student;
+public class Students{
+    public void displayStudent() {
+        System.out.println("Student Name: Ankush Kashyap");
+        System.out.println("Roll Number: 17");
+    }
+}
